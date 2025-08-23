97f7cba889470a9dcb5926ede50f2be8ffbdde83
WakHerv7 <wakam.hermann@gmail.com>
2025-08-23 03:41:01 +0100

    fix/cors-headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,56 +1,64 @@
 /** @type {import('next').NextConfig} */
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '@': path.resolve(__dirname, './'),
-        };
+	webpack: (config) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			"@": path.resolve(__dirname, "./"),
+		};
 
-        return config;
-    },
-    images: {
-            remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            },
-        ],
-    },
-    reactStrictMode: false,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-    async redirects() {
-        return [
-          {
-            source: "/",
-            destination: "/login",
-            permanent: false,
-          },
-          {
-            source: "/dashboard",
-            destination: "/dashboard/v1/home",
-            permanent: false,
-          },
-          {
-            source: "/dashboard/v1",
-            destination: "/dashboard/v1/home",
-            permanent: false,
-          },
-          {
-            source: "/dashboard/v2",
-            destination: "/dashboard/v1/home",
-            permanent: false,
-          },
-        ];
-    },
+		return config;
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
+	},
+	reactStrictMode: false,
+	sassOptions: {
+		includePaths: [path.join(__dirname, "styles")],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/login",
+				permanent: false,
+			},
+		];
+	},
+	async headers() {
+		return [
+			{
+				// Match all API routes
+				source: "/api/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "https://cartevo.co",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,OPTIONS,POST,PUT,DELETE",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type,Authorization",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
