9646ff6836ded493ab2182c5a2dc38a43ce4e246
WakHerv7 <wakam.hermann@gmail.com>
2025-09-25 14:30:17 +0100

    update/ loyaut cookies and sandbox switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 					href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Syne:wght@400..800&display=swap"
 					rel="stylesheet"
 				/>
+				{/* Start cookieyes banner */}
+				<script
+					id="cookieyes"
+					type="text/javascript"
+					src="https://cdn-cookieyes.com/client_data/40e31a15e268dd0908dc5c7a/script.js"
+				></script>
+				{/* End cookieyes banner */}
 			</head>
 			<body className="">
 				{/* {children} */}
